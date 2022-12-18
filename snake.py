d53b969c6c408ca0b24dc2bdc94ee5e95389277b
UP917881 <up917881@myport.ac.uk>
2022-12-18 18:09:45 +0300

    major update, check in a bit

--- snake.py
+++ snake.py
@@ -3,6 +3,8 @@ import random, time, turtle
 # Scores
 pScore = 0
 hScore = 0
+score = 0
+body = []
 
 # Window
 screen = turtle.Screen()
@@ -12,14 +14,38 @@ screen.bgcolor('black')
 # Size of the window
 screen.setup(800, 800)
 
+# Smooth animation
+screen.tracer(0)
+
 # Snake
 snake = turtle.Turtle()
 snake.shape('square')
 snake.color('white')
+snake.speed(0)
+snake.goto(0,0)
 
 # No drawing
 snake.penup()
 snake.direction = "stop"
+
+
+# Food
+food = turtle.Turtle()
+food.shape("circle")
+food.color("orange")
+food.penup()
+food.speed(0)
+food.goto(0, 100)
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
 
 def up():
     snake.direction = "up"
@@ -57,5 +83,83 @@ def move():
 
 while True:
     screen.update()
+
+
+        # Check for a collision with the border
+    if snake.xcor()>390 or snake.xcor()<-390 or snake.ycor()>390 or snake.ycor()<-390:
+        time.sleep(1)
+        snake.goto(0,0)
+        snake.direction = "stop"
+        # Hide the segments
+        for segment in body:
+            segment.goto(1000, 1000)
+        
+        # Clear the segments list
+        body.clear()
+
+        # Reset the score
+        score = 0
+
+        # Reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, hScore), align="center", font=("Courier", 24, "normal")) 
+
+    if snake.distance(food) < 18:
+        x = random.randint(-390, 390)
+        y = random.randint(-390, 390)
+        food.goto(x, y)
+
+        health = turtle.Turtle()
+        health.speed(0)
+        health.shape("square")
+        health.color("blue")
+        health.penup()
+        body.append(health)
+
+        score += 10
+        if score >= hScore:
+            hScore = score
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, hScore), align="center", font=("Courier", 24, "normal")) 
+
+    
+    for i in range(len(body)-1, 0, -1):
+        x = body[i - 1].xcor()
+        y = body[i - 1].ycor()
+        body[i].goto(x, y)
+
+    if len(body) > 0:
+        x = snake.xcor()
+        y = snake.ycor()
+        body[0].goto(x, y)
+
+    time.sleep(.05)
     move()
-    time.sleep(1)
+
+
+    # Check for head collision with the body segments
+    for j in body:
+        if j.distance(snake) < 20:
+            time.sleep(1)
+            snake.goto(0,0)
+            snake.direction = "stop"
+        
+            # Hide the segments
+            for segment in body:
+                segment.goto(1000, 1000)
+        
+            # Clear the segments list
+            body.clear()
+
+            # Reset the score
+            score = 0
+
+            # Reset the delay
+            delay = 0.1
+        
+            # Update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, hScore), align="center", font=("Courier", 24, "normal"))
+ 
